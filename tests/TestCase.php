050e3e9fc652b0035b069d3e7849fdacd5d6b8a4
thurstanjb@icloud.com <c0ath00k>
2019-03-02 20:47:56 +0000

    Add testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,29 @@
 
 namespace Tests;
 
+use App\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $user;
+
+    protected function signIn($user = null){
+        $this->user = $user ?: create(User::class);
+
+        $this->actingAs($this->user);
+
+        return $this;
+    }
+
+    protected function signInUser($user = null){
+        $this->user = $user ?: create(User::Class);
+
+        $this->actingAs($this->user);
+
+        return $this;
+    }
+
 }
